ce44255a496660e3aa6dce00bca8d9a00f1df9af
antonsjostrand <anton.sjostrand@hotmail.se>
2019-03-16 00:31:16 +0100

    Removing faulty database

--- app/src/main/java/com/example/hkr_health/MainActivity.java
+++ app/src/main/java/com/example/hkr_health/MainActivity.java
@@ -6,25 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Variables to handle and create the database.
-    private DatabaseOpenHelper dbOpenHelper;
-    private SQLiteDatabase db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Initialize the openhelper object to be able to create the database.
-        dbOpenHelper = new DatabaseOpenHelper(this);
-
-        db = dbOpenHelper.getReadableDatabase();
-    }
-
-    @Override
-    protected void onDestroy() {
-        dbOpenHelper.close();
-        super.onDestroy();
     }
 
 
