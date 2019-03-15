223fdb39b972fed825e7fd9f7898abd9a1aa56f3
antonsjostrand <anton.sjostrand@hotmail.se>
2019-03-16 00:56:35 +0100

    Changed names

--- app/src/main/java/com/example/hkr_health/LoginActivity.java
+++ app/src/main/java/com/example/hkr_health/LoginActivity.java
@@ -1,15 +1,14 @@
 package com.example.hkr_health;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 
