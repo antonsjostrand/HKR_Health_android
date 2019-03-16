b5037ae73a0af2aae97f4666db37d324ffedd693
antonsjostrand <anton.sjostrand@hotmail.se>
2019-03-16 10:51:59 +0100

    Created login activity skeleton

--- app/src/main/java/com/example/hkr_health/LoginActivity.java
+++ app/src/main/java/com/example/hkr_health/LoginActivity.java
@@ -1,14 +1,55 @@
 package com.example.hkr_health;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //TAG used for logging/debuggning
+    private static final String TAG = "LoginActivity";
+
+    //UI
+    private EditText userName, password;
+    private Button createUserButton, facebookLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userName = findViewById(R.id.usernameET);
+        password = findViewById(R.id.passwordET);
+        createUserButton = findViewById(R.id.createUserButton);
+        facebookLoginButton = findViewById(R.id.facebookLoginButton);
+
+    }
+
+    public void loginButtonPressed(View v){
+        try{
+
+        }catch (Exception e){
+            Log.d(TAG, "createUserButtonPressed: Error: " + e);
+        }
+    }
+
+    public void createUserButtonPressed(View v){
+        try{
+
+        }catch (Exception e){
+            Log.d(TAG, "createUserButtonPressed: Error: " + e);
+        }
+    }
+
+    public void facebookLoginButtonPressed(View v){
+        try{
+
+        }catch (Exception e){
+            Log.d(TAG, "createUserButtonPressed: Error: " + e);
+        }
     }
 
 
